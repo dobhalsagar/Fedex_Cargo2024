1aa21f99b89d7b7e66ff785a85147dbba84bd26d
dobhalsagar <dobhalsagar@SAGAR-PC>
2024-08-06 22:15:08 +0530

    New

--- FedEx-CargoWise/src/test/java/testPackage/NewTestNG.java
+++ FedEx-CargoWise/src/test/java/testPackage/NewTestNG.java
@@ -1,7 +1,6 @@
 package testPackage;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
